952da2e8d0383bf8c4140068973665c2069d1e10
Ioannis Koukotzilas <i.koukotzilas@epsilonhr.gr>
2023-04-21 16:31:40 +0300

    Add hero-detail component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,10 +9,9 @@ import { HEROES } from '../mock-heroes';
 })
 
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
   heroes = HEROES;
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
-
